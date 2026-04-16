60c01ecd784371c5eb2f09939d77cc6cd270cc73
Hudson Hatcher <hds29660>
2026-04-16 13:59:00 -0500

    Database Updates

--- database/init.sql
+++ database/init.sql
@@ -1,78 +1,119 @@
-CREATE DATABASE [NextPlay];
-GO
+CREATE TABLE IF NOT EXISTS public."Sports"
+(
+    sport_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    sport text COLLATE pg_catalog."default",
+    CONSTRAINT "Sports_pkey" PRIMARY KEY (sport_id),
+    CONSTRAINT sport UNIQUE (sport)
+)
 
-USE [NextPlay];
-GO
+TABLESPACE pg_default;
 
-IF NOT EXISTS (SELECT * FROM sys.sql_logins WHERE name = 'admin')
-BEGIN
-    CREATE LOGIN [admin] WITH PASSWORD = 'NextPlay1234', CHECK_POLICY = OFF;
-    ALTER SERVER ROLE [sysadmin] ADD MEMBER [admin];
-END
-GO
+ALTER TABLE IF EXISTS public."Sports"
+    OWNER to admin;
 
-CREATE TABLE Sports (
-    sport_id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
-    sport_name VARCHAR(256)
-);
-GO
+CREATE TABLE IF NOT EXISTS public."Users"
+(
+    user_id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    CONSTRAINT "Users_pkey" PRIMARY KEY (user_id)
+)
 
-CREATE TABLE Users (
-    user_id INT IDENTITY(1,1) NOT NULL PRIMARY KEY ,
-    username varchar ( 256 ), 
-);
-GO
+TABLESPACE pg_default;
 
-CREATE TABLE Leagues (
-    league_id INT IDENTITY(1,1) NOT NULL PRIMARY KEY ,
-    sport_id INT,
-    rankings VARCHAR(2048) NOT NULL,
-    game_results VARCHAR(2048) NOT NULL,
-    FOREIGN KEY(sport_id) REFERENCES Sports(sport_id)
-);
-GO
+ALTER TABLE IF EXISTS public."Users"
+    OWNER to admin;
 
-CREATE TABLE User_Teams (
-    user_team_id INT IDENTITY(1,1) NOT NULL PRIMARY KEY ,
-    user_id INT,
-    league_id INT,
-    roster VARCHAR(2048) NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES Users(user_id),
-    FOREIGN KEY(league_id) REFERENCES Leagues(league_id)
-);
-GO
+CREATE TABLE IF NOT EXISTS public."Leagues"
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    rankings json NOT NULL,
+    commissioner integer NOT NULL,
+    sport integer NOT NULL,
+    CONSTRAINT league_id PRIMARY KEY (id),
+    CONSTRAINT commissioner_user_id FOREIGN KEY (commissioner)
+        REFERENCES public."Users" (user_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT sport_id FOREIGN KEY (sport)
+        REFERENCES public."Sports" (sport_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
 
-CREATE TABLE Game_Results (
-    game_id INT IDENTITY(1,1) NOT NULL PRIMARY KEY ,
-    home_team_id INT,
-    away_team_id INT,
-    game_results VARCHAR(2048) NOT NULL,
-);
-GO
+TABLESPACE pg_default;
 
-CREATE TABLE College (
-    team_id INT IDENTITY(1,1) NOT NULL PRIMARY KEY ,
-    team_name VARCHAR(256),
-    division VARCHAR(256),
-);
-GO
+ALTER TABLE IF EXISTS public."Leagues"
+    OWNER to admin;
+-- Index: fki_sport_id
 
-CREATE TABLE Player (
-    player_id INT IDENTITY(1,1) NOT NULL PRIMARY KEY ,
-    player_name VARCHAR(256),
-    sport_id INT,
-    player_stats VARCHAR(2048) NOT NULL,
-    FOREIGN KEY(sport_id) REFERENCES Sports(sport_id)
-);
-GO
+-- DROP INDEX IF EXISTS public.fki_sport_id;
 
-INSERT INTO [Users] (username)
-VALUES
-('BigCheese');
+CREATE INDEX IF NOT EXISTS fki_sport_id
+    ON public."Leagues" USING btree
+    (sport ASC NULLS LAST)
+    TABLESPACE pg_default;
 
-INSERT INTO [Sports] (sport_name)
-VALUES
-('Football'),
-('Basketball'),
-('Baseball');
+CREATE TABLE IF NOT EXISTS public."UserTeam"
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    user_id integer NOT NULL,
+    league_id integer NOT NULL,
+    roster json,
+    CONSTRAINT "UserTeam_pkey" PRIMARY KEY (id),
+    CONSTRAINT league_id FOREIGN KEY (league_id)
+        REFERENCES public."Leagues" (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT user_id FOREIGN KEY (user_id)
+        REFERENCES public."Users" (user_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
 
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."UserTeam"
+    OWNER to admin;
+
+CREATE TABLE IF NOT EXISTS public."Players"
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    name text COLLATE pg_catalog."default" NOT NULL,
+    team_id integer NOT NULL,
+    CONSTRAINT player_id PRIMARY KEY (id),
+    CONSTRAINT team_id FOREIGN KEY (team_id)
+        REFERENCES public."UserTeam" (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."Players"
+    OWNER to admin;
+
+CREATE TABLE IF NOT EXISTS public."GameResults"
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    home_id integer NOT NULL,
+    away_id integer NOT NULL,
+    results json NOT NULL,
+    CONSTRAINT "GameResults_pkey" PRIMARY KEY (id),
+    CONSTRAINT away_team_id FOREIGN KEY (away_id)
+        REFERENCES public."UserTeam" (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT home_team_id FOREIGN KEY (home_id)
+        REFERENCES public."UserTeam" (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public."GameResults"
+    OWNER to admin;
+
+INSERT INTO public."Sports" (sport)
+	VALUES
+	('Football'), ('Basketball'), ('Baseball');
